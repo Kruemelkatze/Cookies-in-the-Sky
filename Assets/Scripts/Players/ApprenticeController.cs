c5b91de43b6655dde5f6aa41229539417021e743
Sabrina Kletz <skletz@bambi.local>
2017-04-29 20:18:18 +0200

    extends animations of the carot (left, right, jump)

--- Assets/Scripts/Players/ApprenticeController.cs
+++ Assets/Scripts/Players/ApprenticeController.cs
@@ -14,7 +14,11 @@ public class ApprenticeController : MonoBehaviour {
 
 	public bool isGrounded = false;
 
+	public bool moveLeft, moveRight;
+
 	public LayerMask GroundedLayerMask;
+
+	Animator animator;
 
 	void Awake() {
 	}
@@ -24,24 +28,53 @@ public class ApprenticeController : MonoBehaviour {
 		rigid = GetComponent<Rigidbody2D> ();
 		collider2D = GetComponent<Collider2D> ();
 		distToGround = this.collider2D.bounds.extents.y;
+		animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float verticalVelocity = rigid.velocity.y;
-			
+		isGrounded = IsGrounded2D ();
+
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			rigid.velocity = new Vector2 (-Speed, verticalVelocity);
+			animator.SetBool("moveLeft", true);
+			animator.SetBool("moveRight", false);
+			animator.SetBool("idle", false);
+
+
 		} else if (Input.GetKey (KeyCode.RightArrow)) {
 			rigid.velocity = new Vector2 (Speed, verticalVelocity);
-		} else {
-			rigid.velocity = new Vector2 (0, verticalVelocity);
-		}
+			animator.SetBool("moveLeft", false);
+			animator.SetBool("moveRight", true);
+			animator.SetBool("idle", false);
 
-		if (Input.GetKeyDown (KeyCode.Space) && IsGrounded2D()) {
+		} else {
+			animator.SetBool("moveLeft", false);
+			animator.SetBool("moveRight", false);
+
+				animator.SetBool("idle", isGrounded);
+			
+			rigid.velocity = new Vector2 (0, verticalVelocity);
+
+		}
+		
+		if (Input.GetKeyDown (KeyCode.Space) && isGrounded) {
 			float h = rigid.velocity.x;
 			rigid.velocity = new Vector2 (h, JumpForce);
+			//animator.SetBool ("jump", true);
+			//animator.SetBool ("idle", false);
+			//animator.SetBool ("moveLeft", false);
+			//animator.SetBool ("moveRight", 
+			animator.SetBool ("jump", true);
 		}
+
+		animator.SetBool("jump", !isGrounded);
+
+		//if (animator.GetBool ("jump") && isGrounded)
+		//	animator.SetBool ("jump", false);
+
+
 	}
 
 	bool IsGrounded2D() {
