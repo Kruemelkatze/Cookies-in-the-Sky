9a579bff8bea7704b2aa0cca9bfb06dc3137a727
Fabian <schoberf@gmx.net>
2017-04-30 12:45:23 +0200

    Menus

---
+++ Assets/SceneControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("FINAL");
+    }
+
+    public void ToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void ToCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void ToHelp()
+    {
+        SceneManager.LoadScene("Help");
+    }
+
+}
